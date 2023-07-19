b38c1926bf06ae6d2d5887ef048450b90fd8fed8
harshrjjpt <harshrjjpt@gmail.com>
2023-07-19 23:55:19 +0530

    updated files and logic

--- devel/eth-block-meta/schema.sql
+++ devel/eth-block-meta/schema.sql
@@ -1,11 +1,9 @@
-create table block_meta
-(
-    id          integer not null constraint block_meta_pk primary key,
-    at          text,
+CREATE SCHEMA my_schema3;
+
+CREATE TABLE my_schema3.blocks (
+    id          text not null constraint block_meta_pk primary key,
     number      integer,
-    hash        text,
     parent_hash text,
-    uncle_hash  text,
     receipt_root text,
     gas_limit text,
     gas_used text,
@@ -13,16 +11,35 @@ create table block_meta
     size        int
 );
 
-create table cursors
-(
+CREATE TABLE my_schema3.cursors (
     id         text not null constraint cursor_pk primary key,
     cursor     text,
     block_num  bigint,
     block_id   text
 );
 
-create table transactions
-(
+CREATE SCHEMA eth;
+
+CREATE TABLE eth.block_meta (
+    id          text not null constraint block_meta_pk primary key,
+    number      integer,
+    parent_hash text,
+    receipt_root text,
+    gas_limit text,
+    gas_used text,
+    timestamp   text,
+    size        int,
+    nonce       int
+);
+
+CREATE TABLE eth.cursors (
+    id         text not null constraint cursor_pk primary key,
+    cursor     text,
+    block_num  bigint,
+    block_id   text
+);
+
+CREATE TABLE eth.transactions (
     id         text not null constraint transactions_pk primary key,
     status     text,
     gas_used   bigint,
@@ -34,15 +51,16 @@ create table transactions
     from_address   text
 );
 
-create table contracts
-(
+CREATE TABLE eth.contracts (
     id         text not null constraint contracts_pk primary key,
     block_number bigint,
     owner        text,
     address      text,
     transaction_hash text,
     timestamp      text
-
+    
 );
+
+
 
 
